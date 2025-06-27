b4a3ded067b27c42adee12ee360f10814e980862
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-06-28 01:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const name = document.getElementById("name");
+	const email = document.getElementById("email");
+	const password = document.getElementById("password");
+	const confirmPassword = document.getElementById("confirm-password");
+
+	if(name.value === "" || email.value === "" || password.value === "" || confirmPassword.value === ""){
+		return ;
+	}
+
+	if(password.value !== confirmPassword.value) {
+		alert("Passwords do not match");
+	};
+
+	sessionStorage.setItem("name", name.value);
+	sessionStorage.setItem("email", email.value);
+	sessionStorage.setItem("password", password.value);
+	sessionStorage.setItem("confirmPassword", confirmPassword.value);
+	form.reset();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
